fc892ac5d549126549bdb609d8c7baabed421b33
RyshHK <richard.vd.veen@gmail.com>
2026-09-06 18:43:15 +0800

    Update Zsh prompt configuration
    Change the Zsh prompt format to include time and current directory.

--- prompt.zsh
+++ prompt.zsh
@@ -6,9 +6,8 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 FUNCNEST=100
 
 if [[ $(tty) == /dev/tty[0-9]* ]]; then
-    PROMPT='%F{cyan}%n%f %F{yellow}@ %F{green}%m%f: %F{blue}%~%f
-%F{yellow}$%f '
-
+    PROMPT='%F{bright-grey}%T%f: %F{green}%~%f
+%F{yellow}%(?.>.#)%f '
 else
     eval "$(starship init zsh)"
 fi
