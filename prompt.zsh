d36adaacc4119b2ff5954dda4d7c5309af054cf1
RyshHK <richard.vd.veen@gmail.com>
2026-09-06 18:07:34 +0800

    Add conditional prompt based on terminal type
    Set custom prompt for interactive terminal sessions.

--- prompt.zsh
+++ prompt.zsh
@@ -5,4 +5,10 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 FUNCNEST=100
 
-eval "$(starship init zsh)"
+if [[ $(tty) == /dev/tty[0-9]* ]]; then
+    PROMPT='%F{cyan}%n%f %F{yellow}@ %F{green}%m%f: %F{blue}%~%f
+%F{yellow}$%f '
+
+else
+    eval "$(starship init zsh)"
+fi
